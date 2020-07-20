bfab7834307f78d89bffe67264d1ce2edddada36
tbor <btimoteo@outlook.com>
2020-07-20 00:20:33 -0300

    Important changes, add the chat messenger

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,5 @@
-/*!
-    * Start Bootstrap - Creative v6.0.1 (https://startbootstrap.com/themes/creative)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-creative/blob/master/LICENSE)
-    */
-    (function($) {
-  "use strict"; // Start of use strict
-
-  // Smooth scrolling using jQuery easing
+   (function($) {
+  "use strict"; 
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -20,12 +13,11 @@
     }
   });
 
-  // Closes responsive menu when a scroll trigger link is clicked
+
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
   });
 
-  // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
     offset: 75
@@ -44,20 +36,6 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
-  // Magnific popup calls
-  $('#portfolio').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1]
-    },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    }
-  });
+
 
 })(jQuery); // End of use strict
