c0631814f74f2d06c1d83909fd59964291e72df8
tbor <btimoteo@outlook.com>
2020-08-07 14:41:20 -0300

    Fixing

--- js/alerts.js
+++ js/alerts.js
@@ -4,7 +4,7 @@
     Swal.fire({
         icon: 'error',
         title: 'Ups',
-        text: 'No te asustes si algo no anda mal, estamos haciendo cambios en nuestra web',
+        text: 'No te asustes si algo anda mal, estamos haciendo cambios en nuestra web',
         toast: true,
       })
 
