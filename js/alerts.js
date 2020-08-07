d65def6f40c9a4d228baccd35dd3d5aebfe89dbe
tbor <btimoteo@outlook.com>
2020-08-07 14:40:03 -0300

    Fixing Error

--- js/alerts.js
+++ js/alerts.js
@@ -1,14 +1,16 @@
 
- let alerta =   document.querySelector('service');
 
- alerta.addEventListener('click', mantenimiento);
 
- function mantenimiento(){
     Swal.fire({
         icon: 'error',
         title: 'Ups',
-        text: 'No te asustes si algo no anda. Estamos haciendo cambios en nuestra web'
+        text: 'No te asustes si algo no anda mal, estamos haciendo cambios en nuestra web',
+        toast: true,
       })
 
- };
+
+
+
+
+
 
