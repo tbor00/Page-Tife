21aa34bef962ac0404209dddb55841c6a15cb18b
tbor <btimoteo@outlook.com>
2020-08-07 21:03:17 -0300

    Alerts changes

--- js/alerts.js
+++ js/alerts.js
@@ -1,11 +1,31 @@
 
 
+let huawei = document.querySelector('#huawei')
+let alerta = document.querySelector('#error');
+alerta.addEventListener('click', error);
+huawei.addEventListener('click', error);
+
+
+
+function error(){
+  Swal.fire({
+    icon: 'error',
+    title: 'Ups',
+    text: 'Todavía no tienes acceso a esta sección'
+  })
+}
+
+
+
+
+
+
+
 
     Swal.fire({
-        icon: 'error',
-        title: 'Ups',
-        text: 'No te asustes si algo anda mal, estamos haciendo cambios en nuestra web',
-        toast: true,
+        icon: 'warning',
+        title: 'Ey!',
+        text: 'No te asustes si algo anda mal, estamos haciendo cambios en nuestra web'
       })
 
 
