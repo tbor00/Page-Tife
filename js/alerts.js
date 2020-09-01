85ce627ce4aff95b3055fbd55f794f5ddc183679
tbor <btimoteo@outlook.com>
2020-08-31 23:04:27 -0300

    Fix alert

--- js/alerts.js
+++ js/alerts.js
@@ -13,20 +13,7 @@ function error(){
     title: 'Ups',
     text: 'Todavía no tienes acceso a esta sección'
   })
-}
-
-
-
-
-
-
-
-
-    Swal.fire({
-        icon: 'warning',
-        title: 'Ey!',
-        text: 'No te asustes si algo anda mal, estamos haciendo cambios en nuestra web'
-      })
+};
 
 
 
